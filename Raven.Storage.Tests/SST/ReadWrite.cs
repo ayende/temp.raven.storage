3e9a31db3c09680b87827e2596ba5cce2cff59c3
Ayende Rahien <ayende@ayende.com>
2013-03-27 00:24:24 +0200

    Adding SST test

---
+++ Raven.Storage.Tests/SST/ReadWrite.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Raven.Storage.Building;
+using Raven.Storage.Data;
+using Raven.Storage.Reading;
+using Xunit;
+
+namespace Raven.Storage.Tests.SST
+{
+	public class ReadWrite : IDisposable
+	{
+		readonly List<FileStream> shouldHaveBeenDisposed = new List<FileStream>();
+
+		[Fact]
+		public void CanReadValuesBack()
+		{
+			var options = new StorageOptions
+			{
+				ParanoidChecks = true
+			};
+			string name;
+			using (var file = CreateFile())
+			{
+				name = file.Name;
+				using (var tblBuilder = new TableBuilder(options, file, TempStreamGenerator))
+				{
+					for (int i = 0; i < 10; i++)
+					{
+						string k = "tests/" + i.ToString("0000");
+						tblBuilder.Add(k, new MemoryStream(Encoding.UTF8.GetBytes(k)));
+					}
+
+					tblBuilder.Finish();
+					file.Flush(true);
+				}
+			}
+
+			using (var mmf = MemoryMappedFile.CreateFromFile(name, FileMode.Open))
+			{
+				var length = new FileInfo(name).Length;
+				var table = new Table(options, new FileData(mmf, length));
+				using (var iterator = table.CreateIterator(new ReadOptions()))
+				{
+					for (int i = 0; i < 10; i++)
+					{
+						string k = "tests/" + i.ToString("0000");
+						iterator.Seek(k);
+						Assert.True(iterator.IsValid);
+						using (var stream = iterator.CreateValueStream())
+						using (var reader = new StreamReader(stream))
+						{
+							Assert.Equal(k, reader.ReadToEnd());
+						}
+					}
+				}
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.NoInlining)]
+		private FileStream CreateFile()
+		{
+			var stackTrace = new StackTrace();
+			var f = File.Create(stackTrace.GetFrame(stackTrace.FrameCount - 1).GetMethod().Name + ".rsst");
+
+			shouldHaveBeenDisposed.Add(f);
+
+			return f;
+		}
+
+
+		private Stream TempStreamGenerator()
+		{
+			var s = new FileStream(Path.GetTempFileName(), FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite, 4096,
+			                      FileOptions.SequentialScan | FileOptions.DeleteOnClose);
+
+			shouldHaveBeenDisposed.Add(s);
+
+			return s;
+		}
+
+		public void Dispose()
+		{
+			foreach (var stream in shouldHaveBeenDisposed)
+			{
+				Assert.False(stream.CanRead);
+			}
+		}
+	}
+}
--- Raven.Storage.Tryouts/Program.cs
+++ Raven.Storage.Tryouts/Program.cs
@@ -19,63 +19,7 @@ namespace Raven.Storage.Tryouts
 	}
 		static void Main()
 		{
-			//uint x = 254262435u;
 
-			//Console.WriteLine(x);
-			//Console.WriteLine(RotateRight(x, 17));
-			//uint a = (x >> 17);
-			//uint b = (x << 15);
-			//Console.WriteLine(a + " " + b+ " " + (a | b));
-			
-			//// OUTPUT:
-			//// 254262435
-			//// 1939 3729883136 3729885075
-
-			//var bloomFilterBuilder = new BloomFilterBuilder(10, 6, new BloomFilterPolicy(caseInsensitive: false));
-			//bloomFilterBuilder.CreateFilter(new List<Slice>{"test"}, new MemoryStream() );
-
-			new BloomFilterTest().CanProperlyMatch();
-
-			//var options = new StorageOptions
-			//	{
-			//		ParanoidChecks = true
-			//	};
-			//using (var file = File.Create("test2.sst"))
-			//{
-			//	using (var tblBuilder = new TableBuilder(options, file, TempStreamGenerator))
-			//	{
-			//		for (int i = 0; i < 1; i++)
-			//		{
-			//			string k = "tests/" + i.ToString("0000");
-			//			tblBuilder.Add(k, new MemoryStream(Encoding.UTF8.GetBytes(k)));
-			//		}
-
-			//		tblBuilder.Finish();
-			//		file.Flush(true);
-			//	}
-			//}
-
-			//using (var mmf = MemoryMappedFile.CreateFromFile("test2.sst", FileMode.Open))
-			//{
-			//	var length = new FileInfo("test2.sst").Length;
-			//	var table = new Table(options, new FileData(mmf, length));
-			//	using (var iterator = table.CreateIterator(new ReadOptions()))
-			//	{
-			//		iterator.Seek("teSTS/0005");
-			//		Console.WriteLine(iterator.IsValid);
-			//		using(var stream = iterator.CreateValueStream())
-			//		using(var reader = new StreamReader(stream))
-			//		{
-			//			Console.WriteLine(reader.ReadToEnd());
-			//		}
-			//	}
-			//}
-		}
-
-		private static Stream TempStreamGenerator()
-		{
-			return new FileStream(Path.GetTempFileName(), FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite, 4096,
-			                      FileOptions.SequentialScan | FileOptions.DeleteOnClose);
 		}
 	}
 }
