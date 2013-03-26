d78824ce2bb6109a8761e9a0fc41098ca4c43e32
Ayende Rahien <ayende@ayende.com>
2013-03-26 21:11:20 +0200

    Adding tests for case insensitive hashing

---
+++ Raven.Storage.Tests/CaseInsensitiveHashing.cs
@@ -0,0 +1,32 @@
+﻿using Raven.Storage.Filtering;
+using Xunit;
+
+namespace Raven.Storage.Tests
+{
+	public class CaseInsensitiveHashing
+	{
+		[Fact]
+		public void CanHashValueAndGetSameResult()
+		{
+			var hash1 = Bloom.HashCaseInsensitive("tESt");
+			var hash2 = Bloom.HashCaseInsensitive("test");
+			Assert.Equal(hash1, hash2);
+		}
+
+		[Fact]
+		public void CanHashValueAndGetSameResultNotEvenlyDividedBy4()
+		{
+			var hash1 = Bloom.HashCaseInsensitive("tesT3");
+			var hash2 = Bloom.HashCaseInsensitive("teSt3");
+			Assert.Equal(hash1, hash2);
+		}
+
+		[Fact]
+		public void DifferentValuesDifferentHashes()
+		{
+			var hash1 = Bloom.HashCaseInsensitive("test1");
+			var hash2 = Bloom.HashCaseInsensitive("test2");
+			Assert.NotEqual(hash1, hash2);
+		}
+	}
+}
