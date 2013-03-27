811214d84bd0eb6f613587f3e2d24d4279f1d432
Ayende Rahien <ayende@ayende.com>
2013-03-27 11:57:23 +0200

    Starting to port the skip list

---
+++ Raven.Storage.Tests/Memtable/SkipListTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Raven.Storage.Memtable;
+using Xunit;
+using System.Linq;
+
+namespace Raven.Storage.Tests.Memtable
+{
+	public class SkipListTests
+	{
+		[Fact]
+		public void Empty()
+		{
+			var skiplist = new SkipList<string>(string.Compare);
+			Assert.False(skiplist.Contains("10"));
+
+			var iterator = skiplist.NewIterator();
+			Assert.False(iterator.IsValid);
+			iterator.SeekToFirst();
+			Assert.False(iterator.IsValid);
+			iterator.Seek("100");
+			Assert.False(iterator.IsValid);
+			iterator.SeekToLast();
+			Assert.False(iterator.IsValid);
+		}
+
+		[Fact]
+		public void InsertAndLookup()
+		{
+			const int N = 2000;
+			const int R = 5000;
+			var rnd = new Random(1000);
+			var skiplist = new SkipList<string>(string.Compare);
+			var keys = new List<string>();
+			for (int i = 0; i < N; i++)
+			{
+				var key = rnd.Next() % R;
+				var item = key.ToString(CultureInfo.InvariantCulture);
+				if (keys.Contains(item) == false)
+				{
+					keys.Add(item);
+					skiplist.Insert(item);
+				}
+			}
+
+			for (int i = 0; i < R; i++)
+			{
+				var item = i.ToString(CultureInfo.InvariantCulture);
+				if (skiplist.Contains(item))
+				{
+					Assert.True(keys.Contains(item));
+				}
+				else
+				{
+					Assert.False(keys.Contains(item));
+				}
+			}
+
+			var orderedKeys = keys.OrderBy(x => x).ToList();
+
+
+			// iterator tests
+			{
+				var iter = skiplist.NewIterator();
+				Assert.False(iter.IsValid);
+
+				iter.Seek("0");
+				Assert.True(iter.IsValid);
+				Assert.Equal(orderedKeys.First(), iter.Key);
+
+				iter.SeekToFirst();
+				Assert.True(iter.IsValid);
+				Assert.Equal(orderedKeys.First(), iter.Key);
+
+				iter.SeekToLast();
+				Assert.True(iter.IsValid);
+				Assert.Equal(orderedKeys.Last(), iter.Key);
+			}
+			// forward iteration
+			{
+				for (int i = 0; i < R; i++)
+				{
+					var iter = skiplist.NewIterator();
+					var item = i.ToString(CultureInfo.InvariantCulture);
+					iter.Seek(item);
+
+					var indexOf = orderedKeys.FindIndex(s => String.Compare(item, s, StringComparison.Ordinal)  <= 0);
+					for (int j = 0; j < 3; j++)
+					{
+						if (indexOf == -1 || indexOf >= orderedKeys.Count)
+						{
+							Assert.False(iter.IsValid);
+							break;
+						}
+						Assert.True(iter.IsValid);
+						Assert.Equal(orderedKeys[indexOf], iter.Key);
+						indexOf++;
+						iter.Next();
+					}
+				}
+			}
+
+			// backward iteration
+			{
+				var iter = skiplist.NewIterator();
+				iter.SeekToLast();
+				for (int j = orderedKeys.Count - 1; j >= 0; j--)
+				{
+					Assert.True(iter.IsValid);
+					Assert.Equal(orderedKeys[j], iter.Key);
+					iter.Prev();
+				}
+				Assert.False(iter.IsValid);
+			}
+		}
+	}
+}
---
+++ Raven.Storage/Memtable/SkipList.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Raven.Storage.Memtable
+{
+	/// <summary>
+	///  Thread safety
+	///  -------------
+	/// 
+	///  Writes require external synchronization, most likely a mutex.
+	///  Reads require a guarantee that the SkipList will not be destroyed
+	///  while the read is in progress.  Apart from that, reads progress
+	///  without any internal locking or synchronization.
+	/// </summary>
+	public class SkipList<TKey>
+		where TKey : class
+	{
+		private readonly Comparison<TKey> _comparer;
+		public const int SkipListMaxHeight = 12;
+
+		private Node head = new Node(default(TKey), SkipListMaxHeight);
+
+		// Modified only by Insert, allowed to be read racily by readers
+		private int _maxHeight = 1;
+
+		// Only used by Insert, which require externa syncronizastion
+		Random _rnd = new Random();
+
+		public int MaxHeight
+		{
+			get { return Volatile.Read(ref _maxHeight); }
+		}
+
+		public SkipList(Comparison<TKey> comparer)
+		{
+			_comparer = comparer;
+		}
+
+		public bool Contains(TKey key)
+		{
+			var x = FindGreaterOrEqual(key, null);
+			return (x != null && Equal(key, x.Key));
+		}
+
+		public void Insert(TKey key)
+		{
+			// TODO(opt): We can use a barrier-free variant of FindGreaterOrEqual() here since Insert() is externally synchronized.
+			Node[] prev = new Node[SkipListMaxHeight];
+			Node x = FindGreaterOrEqual(key, prev);
+
+			// Our data structure does not allow duplicate insertion
+			if (x != null && Equal(key, x.Key))
+				throw new InvalidOperationException("Key already exists");
+
+			int height = RandomHeight();
+			if (height > MaxHeight)
+			{
+				for (int i = MaxHeight; i < height; i++)
+				{
+					prev[i] = head;
+				}
+
+				// It is ok to mutate max_height_ without any synchronization
+				// with concurrent readers.  A concurrent reader that observes
+				// the new value of max_height_ will see either the old value of
+				// new level pointers from head_ (NULL), or a new value set in
+				// the loop below.  In the former case the reader will
+				// immediately drop to the next level since NULL sorts after all
+				// keys.  In the latter case the reader will use the new node.
+				_maxHeight = height;
+			}
+
+			x = new Node(key,height);
+			for (int i = 0; i < height; i++)
+			{
+				// NoBarrier_SetNext() suffices since we will add a barrier when
+				// we publish a pointer to "x" in prev[i].
+				x.SetNextWithNoBarrier(i, prev[i].GetNextWithNoBarrier(i));
+				prev[i].SetNext(i, x);
+			}
+		}
+
+		public Node FindGreaterOrEqual(TKey key, Node[] prev)
+		{
+			Node x = head;
+			int level = MaxHeight - 1;
+			while (true)
+			{
+				var next = x.Next(level);
+				if (KeyIsAfterNode(key, next))
+				{
+					// Keep searching in this list
+					x = next;
+				}
+				else
+				{
+					if (prev != null)
+						prev[level] = x;
+					if (level == 0)
+					{
+						return next;
+					}
+					// Switch to next list
+					level--;
+				}
+			}
+		}
+
+		public Node FindLessThan(TKey key)
+		{
+			Node x = head;
+			int level = MaxHeight - 1;
+			while (true)
+			{
+				Debug.Assert(x == head || _comparer(x.Key, key) < 0);
+				Node next = x.Next(level);
+				if (next == null || _comparer(next.Key, key) >= 0)
+				{
+					if (level == 0)
+					{
+						return x;
+					}
+					// Switch to next list
+					level--;
+				}
+				else
+				{
+					x = next;
+				}
+			}
+		}
+
+		public Node FindLast()
+		{
+			Node x = head;
+			int level = MaxHeight - 1;
+			while (true)
+			{
+				Node next = x.Next(level);
+				if (next == null)
+				{
+					if (level == 0)
+					{
+						return x;
+					}
+					// Switch to next list
+					level--;
+				}
+				else
+				{
+					x = next;
+				}
+			}
+		}
+
+
+		private bool Equal(TKey a, TKey b)
+		{
+			return _comparer(a, b) == 0;
+		}
+
+		private bool KeyIsAfterNode(TKey key, Node n)
+		{
+			// NULL n is considered infinite
+			return (n != null) && (_comparer(n.Key, key) < 0);
+		}
+
+
+		private int RandomHeight()
+		{
+			// Increase height with probability 1 in kBranching
+			const int branching = 4;
+			int height = 1;
+			while (height < MaxHeight && ((_rnd.Next() % branching) == 0))
+			{
+				height++;
+			}
+			Debug.Assert(height > 0);
+			Debug.Assert(height <= SkipListMaxHeight);
+			return height;
+		}
+
+
+		public class Node
+		{
+			public TKey Key { get; private set; }
+			private Node[] next;
+
+			public Node(TKey key, int height)
+			{
+				Key = key;
+				next = new Node[height];
+			}
+
+			public Node Next(int i)
+			{
+				return Volatile.Read(ref next[i]);
+			}
+
+			public void SetNext(int i, Node val)
+			{
+				Volatile.Write(ref next[i], val);
+			}
+
+			public Node GetNextWithNoBarrier(int i)
+			{
+				return next[i];
+			}
+
+			public void SetNextWithNoBarrier(int i, Node val)
+			{
+				next[i] = val;
+			}
+		}
+
+		public class Iterator
+		{
+			public SkipList<TKey> parent;
+			private Node node;
+
+			public Iterator(SkipList<TKey> parent)
+			{
+				this.parent = parent;
+			}
+
+			public bool IsValid
+			{
+				get
+				{
+					return node != null;
+				}
+			}
+
+			public TKey Key
+			{
+				get
+				{
+					return node.Key;
+				}
+			}
+
+			public void Next()
+			{
+				node = node.Next(0);
+			}
+
+			public void Prev()
+			{
+				// Instead of using explicit "prev" links, we just search for the
+				// last node that falls before key.
+				node = parent.FindLessThan(node.Key);
+				if (node == parent.head)
+				{
+					node = null;
+				}
+			}
+
+			public void Seek(TKey key)
+			{
+				node = parent.FindGreaterOrEqual(key, null);
+			}
+
+			public void SeekToFirst()
+			{
+				node = parent.head.Next(0);
+			}
+
+			public void SeekToLast()
+			{
+				node = parent.FindLast();
+				if (node == parent.head)
+				{
+					node = null;
+				}
+			}
+		}
+
+		public Iterator NewIterator()
+		{
+			return new Iterator(this);
+		}
+	}
+}
