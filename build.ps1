17d6b997c8627d68aa124286a4b048b6eab41331
Ayende Rahien <ayende@ayende.com>
2013-06-21 11:57:35 +0300

    Trivial build script

---
+++ build.ps1
@@ -0,0 +1,4 @@
+ C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild
+ cls
+ .\packages\xunit.runners.1.9.1\tools\xunit.console.clr4.exe .\Raven.Storage.Tests\bin\Debug\Raven.Storage.Tests.dll
+ .\packages\xunit.runners.1.9.1\tools\xunit.console.clr4.exe .\Raven.Aggregation.Tests\bin\Debug\Raven.Aggregation.Tests.dll
