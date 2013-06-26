6b2d5713dabfc6949def4e56014696c2aaa371d6
Arkadiusz Palinski <arek@ais.pl>
2013-06-26 11:28:30 +0200

    Much better compaction test. Fixing files overlapping detection for empty slices (needed to fix CompactRange(null, null))

--- Raven.Storage.Tests/Compactions/CompactionTests.cs
+++ Raven.Storage.Tests/Compactions/CompactionTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Raven.Storage.Impl;
 
 namespace Raven.Storage.Tests.Compactions
 {
@@ -15,22 +16,62 @@ namespace Raven.Storage.Tests.Compactions
 		{
 			using (var storage = await NewStorageAsync())
 			{
-				for (int i = 0; i < 3; i++)
-				{
-					var writeBatch = new WriteBatch();
-					writeBatch.Put("p", new MemoryStream(Encoding.UTF8.GetBytes("begin")));
-					await storage.Writer.WriteAsync(writeBatch);
+				Assert.Equal(Config.MaxMemCompactLevel, 2); // Just to make sure - here we relay on that
 
-					writeBatch = new WriteBatch();
-					writeBatch.Put("q", new MemoryStream(Encoding.UTF8.GetBytes("end")));
-					await storage.Writer.WriteAsync(writeBatch);
-
-					await storage.Commands.CompactMemTableAsync();
-				}
+				await MakeTables(storage, 3, "p", "q");
 
 				var statistics = await storage.Commands.GetStatisticsAsync();
-
 				AssertNumberOfFilesPerLevel(statistics, 1, 1, 1, 0, 0, 0, 0);
+
+				// Compaction range falls before files
+				await storage.Commands.CompactRangeAsync("", "c");
+				statistics = await storage.Commands.GetStatisticsAsync();
+				AssertNumberOfFilesPerLevel(statistics, 1, 1, 1, 0, 0, 0, 0);
+
+				// Compaction range falls after files
+				await storage.Commands.CompactRangeAsync("r", "z");
+				statistics = await storage.Commands.GetStatisticsAsync();
+				AssertNumberOfFilesPerLevel(statistics, 1, 1, 1, 0, 0, 0, 0);
+
+				// Compaction range overlaps files
+				await storage.Commands.CompactRangeAsync("p1", "p9");
+				statistics = await storage.Commands.GetStatisticsAsync();
+				AssertNumberOfFilesPerLevel(statistics, 0, 0, 1, 0, 0, 0, 0);
+
+				// Populate a different range
+				await MakeTables(storage, 3, "c", "e");
+
+				statistics = await storage.Commands.GetStatisticsAsync();
+				AssertNumberOfFilesPerLevel(statistics, 1, 1, 2, 0, 0, 0, 0);
+
+				// Compact just the new range
+				await storage.Commands.CompactRangeAsync("b", "f");
+				statistics = await storage.Commands.GetStatisticsAsync();
+				AssertNumberOfFilesPerLevel(statistics, 0, 0, 2, 0, 0, 0, 0);
+
+				// Compact all
+				await MakeTables(storage, 1, "a", "z");
+				statistics = await storage.Commands.GetStatisticsAsync();
+				AssertNumberOfFilesPerLevel(statistics, 0, 1, 2, 0, 0, 0, 0);
+				await storage.Commands.CompactRangeAsync(null, null);
+				statistics = await storage.Commands.GetStatisticsAsync();
+				AssertNumberOfFilesPerLevel(statistics, 0, 0, 1, 0, 0, 0, 0);
+			}
+		}
+
+		private async Task MakeTables(Storage storage, int n, string small, string large)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				var writeBatch = new WriteBatch();
+				writeBatch.Put(small, new MemoryStream(Encoding.UTF8.GetBytes("begin")));
+				await storage.Writer.WriteAsync(writeBatch);
+
+				writeBatch = new WriteBatch();
+				writeBatch.Put(large, new MemoryStream(Encoding.UTF8.GetBytes("end")));
+				await storage.Writer.WriteAsync(writeBatch);
+
+				await storage.Commands.CompactMemTableAsync();
 			}
 		}
 
