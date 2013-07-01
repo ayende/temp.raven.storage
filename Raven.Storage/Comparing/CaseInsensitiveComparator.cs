eed0e026bf5afaf757f00638ad68e8d325f8ff16
Arkadiusz Palinski <arek@ais.pl>
2013-07-01 10:11:48 +0200

    Pointers used in CaseInsensitiveComparator to make it faster

--- Raven.Storage/Comparing/CaseInsensitiveComparator.cs
+++ Raven.Storage/Comparing/CaseInsensitiveComparator.cs
@@ -20,26 +20,39 @@ namespace Raven.Storage.Comparing
 		{
 			var minLen = a.Count <= b.Count ? a.Count : b.Count;
 
-			for (int i = 0; i < minLen; i++)
+			unsafe
 			{
-				var cha = (char)a.Array[a.Offset + i];
-				var chb = (char)b.Array[b.Offset + i];
-
-				if (((cha >= 'a' && cha <= 'z') || (cha >= 'A' && cha <= 'Z')) && ((chb >= 'a' && chb <= 'z') || (chb >= 'A' && chb <= 'Z')))
+				fixed (byte* ap = a.Array)
 				{
-					var diff = cha - chb;
-					if (diff == 0 || diff == 32 || diff == -32)
-						continue;
+					fixed (byte* bp = b.Array)
+					{
+						byte* aPtr = ap + a.Offset;
+						byte* bPtr = bp + b.Offset;
 
-					return diff;
-				}
-				else
-				{
-					var diff = cha - chb;
-					if (diff != 0)
-						return diff;
+						for (int i = 0; i < minLen; i++)
+						{
+							if (((*aPtr >= 'a' && *aPtr <= 'z') || (*aPtr >= 'A' && *aPtr <= 'Z')) && ((*bPtr >= 'a' && *bPtr <= 'z') || (*bPtr >= 'A' && *bPtr <= 'Z')))
+							{
+								var diff = *aPtr - *bPtr;
+								if (diff == 0 || diff == 32 || diff == -32)
+									continue;
+
+								return diff;
+							}
+							else
+							{
+								var diff = *aPtr - *bPtr;
+								if (diff != 0)
+									return diff;
+							}
+
+							aPtr++;
+							bPtr++;
+						}
+					}
 				}
 			}
+			
 
 			return a.Count - b.Count;
 		}
