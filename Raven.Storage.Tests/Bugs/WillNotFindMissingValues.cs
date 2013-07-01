c8730954422405d562467bc108f8de4d30ac42e2
Ayende Rahien <ayende@ayende.com>
2013-07-01 12:40:05 +0300

    Adding FAILING test

---
+++ Raven.Storage.Tests/Bugs/WillNotFindMissingValues.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Raven.Storage.Comparing;
+using Xunit;
+
+namespace Raven.Storage.Tests.Bugs
+{
+	public class WillNotFindMissingValues : StorageTestBase
+	{
+		[Fact]
+		public async Task WillNotFindMissingValue()
+		{
+			using (var storage = await NewStorageAsync(new StorageOptions()))
+			{
+				var writeBatch = new WriteBatch();
+				writeBatch.Put("system/config", new MemoryStream());
+				await storage.Writer.WriteAsync(writeBatch);
+
+				Assert.Null(storage.Reader.Read("system/Test"));
+			}
+		}
+	}
+}
